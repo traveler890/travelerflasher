0c35ccef386d605a6abc37d82f621ca1c664c163
TravelerOS <traveleros44@gmail.com>
2025-07-05 16:44:54 -0400

    Create iso_tools.py

---
+++ utils/iso_tools.py
@@ -0,0 +1,8 @@
+import hashlib
+
+def sha256_file(path):
+    hash_sha256 = hashlib.sha256()
+    with open(path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
